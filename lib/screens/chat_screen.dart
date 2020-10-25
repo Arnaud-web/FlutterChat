720e95611f6a626346f1bff5f17c32db0fab937b
Arnaud <mrandriamihaja2@gmail.com>
2020-10-25 23:15:10 +0300

    stable test firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,21 +5,35 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(3),
-          child: Text("Bonjour"),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection("chats/A4SZZpeim3WmOxToh4l4/messages")
+            .snapshots(),
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) => Container(
+                    padding: EdgeInsets.all(3),
+                    child: Text(documents[index]['text']),
+                  ));
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){
-          Firestore.instance.collection("chats/A4SZZpeim3WmOxToh4l4/messages").snapshots().listen((data) {
-            print(data.documents[0]['text']);
-            print("ok");
-           });
-        },),
+        onPressed: () {
+          Firestore.instance
+              .collection("chats/A4SZZpeim3WmOxToh4l4/messages")
+              .add(
+            {'text': 'text add by btn'},
+          );
+        },
+      ),
     );
   }
 }
