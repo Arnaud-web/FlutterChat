a8b46d08fd5010ab8ecbaf4f5a517c7866e31df4
Arnaud <mrandriamihaja2@gmail.com>
2020-10-27 23:22:26 +0300

    multiple devise and display name

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(this.message);
+  MessageBubble(this.message, this.userName, this.isMe, {this.key});
   final String message;
-
+  final bool isMe;
+  final Key key;
+  final String userName;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-              color: Theme.of(context).accentColor,
-              borderRadius: BorderRadius.circular(16)),
+              color: isMe ? Colors.purple : Theme.of(context).accentColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+                bottomRight: !isMe ? Radius.circular(0) : Radius.circular(12),
+              )),
           width: 140,
           padding: EdgeInsets.symmetric(
             vertical: 10,
@@ -21,9 +29,27 @@ class MessageBubble extends StatelessWidget {
             vertical: 4,
             horizontal: 8,
           ),
-          child: Text(
-            message,
-            style: Theme.of(context).accentTextTheme.headline6,
+          child: Column(
+            crossAxisAlignment:
+                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              Text(
+                userName,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: isMe ? Colors.white : Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.white,
+                  fontSize: 16,
+                ),
+                //  isMe ? Theme.of(context).accentTextTheme.headline6 : Theme.of(context).accentTextTheme.headline6,
+              ),
+            ],
           ),
         ),
       ],
